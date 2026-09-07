a4fbbe0e25a0bc3685e36f1f625660ff294b0284
Christian B. <Chrisbee76@UltimateWisdom.Net>
2026-09-07 12:57:42 +0200

    Remove layout handling from editor base

--- Textbase.Host/Components/Common/TextbaseGuidEditorViewBase.cs
+++ Textbase.Host/Components/Common/TextbaseGuidEditorViewBase.cs
@@ -1,6 +1,4 @@
-using Microsoft.AspNetCore.Components;
 using Radzen;
-using Textbase.Host.Components.Infrastructure;
 using Uwn.Blazor.Components.Abstractions.Querying;
 using Uwn.Blazor.Models.Common;
 using Uwn.Blazor.Models.ViewModels.Abstractions.Querying;
@@ -14,10 +12,6 @@ public abstract class TextbaseGuidEditorViewBase<TViewModel, TDTO, TModel, TFilt
 	where TModel : class, TDTO
 	where TFilter : class
 {
-	[CascadingParameter]
-	private MainLayout MainLayout { get; set; } = default!;
-
-	protected abstract string Header { get; }
 	protected virtual string UnauthorizedPath => StaticRoutes.Home;
 
 	protected override async Task OnParametersSetAsync()
@@ -25,7 +19,6 @@ public abstract class TextbaseGuidEditorViewBase<TViewModel, TDTO, TModel, TFilt
 		try
 		{
 			await base.OnParametersSetAsync();
-			MainLayout.SetHeader(Header);
 		}
 		catch (UnauthorizedAccessException exception)
 		{
