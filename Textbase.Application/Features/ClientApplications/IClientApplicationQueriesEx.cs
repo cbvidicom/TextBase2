206954f051ca7bb4ea042743c0f2e3d998d5aafe
Christian B. <Chrisbee76@UltimateWisdom.Net>
2026-09-07 12:40:52 +0200

    Remove client-only query extension

--- Textbase.Application/Features/ClientApplications/IClientApplicationQueriesEx.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Textbase.Application.Features.ClientApplications;
-
-public partial interface IClientApplicationQueries
-{
-	Task<IReadOnlyDictionary<Guid, ClientApplicationReferenceCounts>> GetReferenceCountsAsync(
-		IReadOnlyCollection<Guid> clientApplicationGuids, CancellationToken cancellationToken = default);
-}
