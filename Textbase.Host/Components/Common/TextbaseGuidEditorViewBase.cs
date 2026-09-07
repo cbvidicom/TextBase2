fe93edf1aaf4f6c80f6fa3ef7daff4b3f3133d18
Christian B. <Chrisbee76@UltimateWisdom.Net>
2026-09-07 12:53:44 +0200

    Add common Textbase GUID editor base

---
+++ Textbase.Host/Components/Common/TextbaseGuidEditorViewBase.cs
@@ -0,0 +1,49 @@
+using Microsoft.AspNetCore.Components;
+using Radzen;
+using Textbase.Host.Components.Infrastructure;
+using Uwn.Blazor.Components.Abstractions.Querying;
+using Uwn.Blazor.Models.Common;
+using Uwn.Blazor.Models.ViewModels.Abstractions.Querying;
+
+namespace Textbase.Host.Components.Common;
+
+public abstract class TextbaseGuidEditorViewBase<TViewModel, TDTO, TModel, TFilter>
+	: BaseGuidEditorView<TViewModel, TDTO, TModel, TFilter>
+	where TViewModel : GuidEditorViewModel<TDTO, TModel>
+	where TDTO : class
+	where TModel : class, TDTO
+	where TFilter : class
+{
+	[CascadingParameter]
+	private MainLayout MainLayout { get; set; } = default!;
+
+	protected abstract string Header { get; }
+	protected virtual string UnauthorizedPath => StaticRoutes.ClientApplicationList;
+
+	protected override async Task OnParametersSetAsync()
+	{
+		try
+		{
+			await base.OnParametersSetAsync();
+			MainLayout.SetHeader(Header);
+		}
+		catch (UnauthorizedAccessException exception)
+		{
+			CoreAlertService.Show(exception.Message, AlertStyle.Danger);
+			CoreNavigationManager.NavigateTo(UnauthorizedPath);
+		}
+	}
+
+	protected async Task SaveAuthorizedItemAsync(
+		SaveMode mode)
+	{
+		try
+		{
+			await SaveItemAsync(mode);
+		}
+		catch (UnauthorizedAccessException exception)
+		{
+			CoreAlertService.Show(exception.Message, AlertStyle.Danger);
+		}
+	}
+}
