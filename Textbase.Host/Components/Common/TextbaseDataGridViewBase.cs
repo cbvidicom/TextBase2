fa412be5bab787f6ce356ae79c7e932f607fdb88
Christian B. <Chrisbee76@UltimateWisdom.Net>
2026-09-08 11:50:50 +0200

    Set grid headers from shared base

--- Textbase.Host/Components/Common/TextbaseDataGridViewBase.cs
+++ Textbase.Host/Components/Common/TextbaseDataGridViewBase.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Components;
 using Uwn.Blazor.Components.Abstractions.Querying;
 using Uwn.Blazor.Models.ViewModels.Abstractions.Querying;
 using Uwn.Common.Querying;
@@ -10,7 +11,16 @@ public abstract class TextbaseDataGridViewBase<TViewModel, TModel, TFilter>
 	where TModel : class
 	where TFilter : QueryFilterBase, new()
 {
+	[CascadingParameter]
+	private MainLayout MainLayout { get; set; } = default!;
+
 	protected bool IsInitialized { get; private set; }
+
+	protected override async Task OnParametersSetAsync()
+	{
+		await base.OnParametersSetAsync();
+		MainLayout.SetHeader(MainHeader ?? String.Empty);
+	}
 
 	protected override void OnAfterRender(
 		bool firstRender)
