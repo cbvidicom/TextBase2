066cf386af177c4f6e26202e7d65d54bfe200510
Christian B. <Chrisbee76@UltimateWisdom.Net>
2026-09-09 11:19:25 +0200

    Move server query registrations to application common

---
+++ Textbase.Application/Common/ServerQueryServiceCollectionExtensions.cs
@@ -0,0 +1,19 @@
+using Microsoft.Extensions.DependencyInjection;
+using Textbase.Application.Features.ClientApplications;
+using Textbase.Application.Features.Formalities;
+using Textbase.Application.Features.Presentations;
+
+namespace Textbase.Application.Common;
+
+public static class ServerQueryServiceCollectionExtensions
+{
+	public static IServiceCollection AddTextbaseServerQueries(
+		this IServiceCollection services)
+	{
+		services.AddScoped(services => (IClientApplicationServerQueries)services.GetRequiredService<IClientApplicationQueries>());
+		services.AddScoped(services => (IFormalityServerQueries)services.GetRequiredService<IFormalityQueries>());
+		services.AddScoped(services => (IPresentationServerQueries)services.GetRequiredService<IPresentationQueries>());
+
+		return services;
+	}
+}
