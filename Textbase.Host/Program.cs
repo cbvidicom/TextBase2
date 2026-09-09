7e4da06cb305674d29993736313ff6a821fc77d5
Christian B. <Chrisbee76@UltimateWisdom.Net>
2026-09-09 11:15:37 +0200

    Use server query DI extension

--- Textbase.Host/Program.cs
+++ Textbase.Host/Program.cs
@@ -7,9 +7,6 @@ using Microsoft.Identity.Web;
 using Microsoft.Identity.Web.UI;
 using Radzen;
 using Textbase.Application.Common;
-using Textbase.Application.Features.ClientApplications;
-using Textbase.Application.Features.Formalities;
-using Textbase.Application.Features.Presentations;
 using Textbase.Host.Api.Authorization;
 using Textbase.Host.Api.Infrastructure;
 using Textbase.Host.Authorization;
@@ -74,9 +71,7 @@ builder.Services.AddAuthorizationBuilder()
 
 builder.Services.AddTextbaseInfrastructure(connectionString);
 builder.Services.AddTextbaseApplication(includeServerCommands: true);
-builder.Services.AddScoped(services => (IClientApplicationServerQueries)services.GetRequiredService<IClientApplicationQueries>());
-builder.Services.AddScoped(services => (IFormalityServerQueries)services.GetRequiredService<IFormalityQueries>());
-builder.Services.AddScoped(services => (IPresentationServerQueries)services.GetRequiredService<IPresentationQueries>());
+builder.Services.AddTextbaseServerQueries();
 
 builder.Services
 	.AddControllersWithViews(options =>
