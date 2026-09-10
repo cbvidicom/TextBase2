e7e0a1d28afeff3f5d3aa5e3c5f80f1e873bd625
Christian B. <Chrisbee76@UltimateWisdom.Net>
2026-09-10 08:48:56 +0200

    Show TextResource reference counts

--- Textbase.Host/ViewModels/TextResources/TextResourceListViewModel.cs
+++ Textbase.Host/ViewModels/TextResources/TextResourceListViewModel.cs
@@ -1,3 +1,4 @@
+using Radzen;
 using System.Security.Claims;
 using Textbase.Application.Features.TextResources;
 using Textbase.Domain.Models;
@@ -13,11 +14,13 @@ namespace Textbase.Host.ViewModels.TextResources;
 public class TextResourceListViewModel(
 	ITextResourceAuthorization _authorization,
 	ICurrentPrincipalAccessor _currentPrincipalAccessor,
-	ITextResourceQueries textResourceQueries,
+	ITextResourceServerQueries textResourceQueries,
 	ITextResourceEntityFactory _textResourceEntityFactory)
 	: DataGridViewModel<TextResource, TextResourceFilter>(textResourceQueries)
 {
 	private readonly Type ModelType = typeof(TextResource);
+
+	private IReadOnlyDictionary<string, TextResourceReferenceCounts>? _referenceCounts;
 
 	protected override async Task<ViewAuthorizationResult> AuthorizeCreateAsync(
 		CancellationToken cancellationToken = default)
@@ -54,5 +57,36 @@ public class TextResourceListViewModel(
 		return canList
 			? ViewAuthorizationResult.Authorized
 			: ViewAuthorizationResult.Denied(StaticTexts.GetPrincipalNotAuthorizedText(OpType.List, ModelType));
+	}
+
+	protected override async Task AfterLoadDataAsync(
+		LoadDataArgs args)
+	{
+		_referenceCounts = Data is null
+			? null
+			: await textResourceQueries.GetReferenceCountsAsync([.. Data.Select(textResource => textResource.TextKey)]);
+	}
+
+	public int GetClientApplicationCount(
+		string textKey)
+		=> GetReferenceCounts(textKey)?.ClientApplicationCount ?? 0;
+
+	public int GetLocaleCount(
+		string textKey)
+		=> GetReferenceCounts(textKey)?.LocaleCount ?? 0;
+
+	public int GetTranslationCount(
+		string textKey)
+		=> GetReferenceCounts(textKey)?.TranslationCount ?? 0;
+
+	private TextResourceReferenceCounts? GetReferenceCounts(
+		string textKey)
+	{
+		if (_referenceCounts is null)
+			return null;
+
+		return _referenceCounts.TryGetValue(textKey, out TextResourceReferenceCounts? value)
+			? value
+			: null;
 	}
 }
