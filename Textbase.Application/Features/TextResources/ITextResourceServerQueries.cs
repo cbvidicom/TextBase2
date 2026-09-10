291abc5c5cf96ab4240761ee644f017ddd144320
Christian B. <Chrisbee76@UltimateWisdom.Net>
2026-09-10 08:48:12 +0200

    Add TextResource reference counts

---
+++ Textbase.Application/Features/TextResources/ITextResourceServerQueries.cs
@@ -0,0 +1,8 @@
+namespace Textbase.Application.Features.TextResources;
+
+public interface ITextResourceServerQueries
+	: ITextResourceQueries
+{
+	Task<IReadOnlyDictionary<string, TextResourceReferenceCounts>> GetReferenceCountsAsync(
+		IReadOnlyCollection<string> textKeys, CancellationToken cancellationToken = default);
+}
