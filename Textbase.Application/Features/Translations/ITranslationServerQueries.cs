271e84b21178d1562cf38fa317e721b401f65009
Christian B. <Chrisbee76@UltimateWisdom.Net>
2026-09-10 10:59:11 +0200

    Return missing translation model from query

--- Textbase.Application/Features/Translations/ITranslationServerQueries.cs
+++ Textbase.Application/Features/Translations/ITranslationServerQueries.cs
@@ -4,6 +4,6 @@ public interface ITranslationServerQueries
 	: ITranslationQueries
 {
 	Task<bool> HasActiveApplicationAsync(string localeKey, string textKey, CancellationToken cancellationToken = default);
-	Task<IReadOnlyList<MissingTranslationRequirement>> ListMissingTranslationsAsync(
+	Task<IReadOnlyList<MissingTranslation>> ListMissingTranslationsAsync(
 		IReadOnlyCollection<Guid>? clientApplicationGuids, IReadOnlyCollection<string>? localeKeys, CancellationToken cancellationToken = default);
 }
