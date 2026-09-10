1071964107691e2bd5122a719846b8712cb2aa90
Christian B. <Chrisbee76@UltimateWisdom.Net>
2026-09-10 07:27:18 +0200

    Show principal assignment counts

--- Textbase.Host/ViewModels/AuthPrincipals/AuthPrincipalListViewModel.cs
+++ Textbase.Host/ViewModels/AuthPrincipals/AuthPrincipalListViewModel.cs
@@ -1,3 +1,4 @@
+using Radzen;
 using System.Security.Claims;
 using Textbase.Application.Features.AuthPrincipals;
 using Textbase.Domain.Models;
@@ -13,10 +14,13 @@ namespace Textbase.Host.ViewModels.AuthPrincipals;
 public class AuthPrincipalListViewModel(
 	IAuthPrincipalAuthorization _authorization,
 	ICurrentPrincipalAccessor _currentPrincipalAccessor,
-	IAuthPrincipalQueries authPrincipalQueries)
+	IAuthPrincipalQueries authPrincipalQueries,
+	IAuthPrincipalServerQueries _authPrincipalServerQueries)
 	: DataGridViewModel<AuthPrincipal, AuthPrincipalFilter>(authPrincipalQueries)
 {
 	private readonly Type ModelType = typeof(AuthPrincipal);
+
+	private IReadOnlyDictionary<Guid, AuthPrincipalReferenceCounts>? _referenceCounts;
 
 	protected override async Task<ViewAuthorizationResult> AuthorizeReadAsync(
 		CancellationToken cancellationToken = default)
@@ -43,7 +47,37 @@ public class AuthPrincipalListViewModel(
 			: ViewAuthorizationResult.Denied(StaticTexts.GetPrincipalNotAuthorizedText(OpType.List, ModelType));
 	}
 
+	protected override async Task AfterLoadDataAsync(
+		LoadDataArgs args)
+	{
+		_referenceCounts = Data is null
+			? null
+			: await _authPrincipalServerQueries.GetReferenceCountsAsync([.. Data.Select(principal => principal.EntraObjectId)]);
+	}
+
 	public override DataGridRowStyle GetItemStyle(
 		AuthPrincipal item)
 		=> item.IsActive ? DataGridRowStyle.Base : DataGridRowStyle.Danger;
+
+	public int GetClientApplicationCount(
+		Guid entraObjectId)
+	{
+		if (_referenceCounts is null)
+			return 0;
+
+		return _referenceCounts.TryGetValue(entraObjectId, out AuthPrincipalReferenceCounts? value)
+			? value.ClientApplicationCount
+			: 0;
+	}
+
+	public int GetLocaleCount(
+		Guid entraObjectId)
+	{
+		if (_referenceCounts is null)
+			return 0;
+
+		return _referenceCounts.TryGetValue(entraObjectId, out AuthPrincipalReferenceCounts? value)
+			? value.LocaleCount
+			: 0;
+	}
 }
