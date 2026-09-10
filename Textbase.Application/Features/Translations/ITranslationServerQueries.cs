d86ef3598ad6afab7d370eac49316f74ce8ba760
Christian B. <Chrisbee76@UltimateWisdom.Net>
2026-09-10 10:28:37 +0200

    Expose missing translation query

--- Textbase.Application/Features/Translations/ITranslationServerQueries.cs
+++ Textbase.Application/Features/Translations/ITranslationServerQueries.cs
@@ -4,4 +4,6 @@ public interface ITranslationServerQueries
 	: ITranslationQueries
 {
 	Task<bool> HasActiveApplicationAsync(string localeKey, string textKey, CancellationToken cancellationToken = default);
+	Task<IReadOnlyList<MissingTranslationRequirement>> ListMissingTranslationsAsync(
+		IReadOnlyCollection<Guid>? clientApplicationGuids, IReadOnlyCollection<string>? localeKeys, CancellationToken cancellationToken = default);
 }
