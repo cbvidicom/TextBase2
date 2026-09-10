e54930964efe4dadbb9ca8bf64ceb4b052cc2a1c
Christian B. <Chrisbee76@UltimateWisdom.Net>
2026-09-10 06:51:54 +0200

    Remove user creation authorization from list

--- Textbase.Host/ViewModels/AuthPrincipals/AuthPrincipalListViewModel.cs
+++ Textbase.Host/ViewModels/AuthPrincipals/AuthPrincipalListViewModel.cs
@@ -1,11 +1,9 @@
 using System.Security.Claims;
 using Textbase.Application.Features.AuthPrincipals;
-using Textbase.Domain.Enumerations;
 using Textbase.Domain.Models;
 using Textbase.Host.Api.Authorization;
 using Textbase.Host.Authorization;
 using Textbase.Host.Enumerations;
-using Textbase.Infrastructure.Persistence.AuthPrincipals;
 using Uwn.Blazor.Enumerations.Radzen;
 using Uwn.Blazor.Models.Common;
 using Uwn.Blazor.Models.ViewModels.Abstractions.Querying;
@@ -15,23 +13,10 @@ namespace Textbase.Host.ViewModels.AuthPrincipals;
 public class AuthPrincipalListViewModel(
 	IAuthPrincipalAuthorization _authorization,
 	ICurrentPrincipalAccessor _currentPrincipalAccessor,
-	IAuthPrincipalQueries authPrincipalQueries,
-	IAuthPrincipalEntityFactory _authPrincipalEntityFactory)
+	IAuthPrincipalQueries authPrincipalQueries)
 	: DataGridViewModel<AuthPrincipal, AuthPrincipalFilter>(authPrincipalQueries)
 {
 	private readonly Type ModelType = typeof(AuthPrincipal);
-
-	protected override async Task<ViewAuthorizationResult> AuthorizeCreateAsync(
-		CancellationToken cancellationToken = default)
-	{
-		AuthPrincipal principal = _authPrincipalEntityFactory.Create(Guid.Empty, (int)Roles.None, (int)PrincipalStatus.Pending);
-		ClaimsPrincipal user = await _currentPrincipalAccessor.GetUserAsync();
-		bool isAuthorized = await _authorization.CanCreateAsync(principal, user, cancellationToken);
-
-		return isAuthorized
-			? ViewAuthorizationResult.Authorized
-			: ViewAuthorizationResult.Denied(StaticTexts.GetPrincipalNotAuthorizedText(OpType.Create, ModelType));
-	}
 
 	protected override async Task<ViewAuthorizationResult> AuthorizeReadAsync(
 		CancellationToken cancellationToken = default)
