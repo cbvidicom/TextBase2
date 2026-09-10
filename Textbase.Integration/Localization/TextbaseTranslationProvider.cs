a1d4ef805023868d19e1c6e305d9ebbafa7ea079
Christian B. <Chrisbee76@UltimateWisdom.Net>
2026-09-10 12:55:37 +0200

    Use application-specific snapshot endpoint

--- Textbase.Integration/Localization/TextbaseTranslationProvider.cs
+++ Textbase.Integration/Localization/TextbaseTranslationProvider.cs
@@ -1,21 +1,16 @@
-using Textbase.Application.Features.FlatTranslations;
 using DM = Textbase.Domain.Models;
-using Uwn.Common.Querying;
 
 namespace Textbase.Integration.Localization;
 
 public sealed class TextbaseTranslationProvider(
-	IFlatTranslationQueries queries,
+	ITranslationSnapshotClient client,
 	ITranslationStore store)
 {
 	public async Task<IReadOnlyCollection<DM.FlatTranslation>> RefreshAsync(
 		Guid clientApplicationGuid,
 		CancellationToken cancellationToken = default)
 	{
-		FlatTranslationFilter filter = FlatTranslationFilter.All();
-		PagedResponse<DM.FlatTranslation> response = await queries.ListAsync(filter, cancellationToken);
-		IReadOnlyCollection<DM.FlatTranslation> translations = response.Items;
-
+		IReadOnlyList<DM.FlatTranslation> translations = await client.GetAsync(clientApplicationGuid, cancellationToken);
 		await store.SetAsync(translations, cancellationToken);
 		return translations;
 	}
